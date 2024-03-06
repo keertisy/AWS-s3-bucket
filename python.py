5f4ab59e4a1d11e62fdcef800f4a8baf06546ac7
keertisy <keerti.yalasangimath@gmail.com>
2024-03-06 15:07:26 +0530

    update python file

--- python.py
+++ python.py
@@ -1,13 +1,19 @@
 import boto3
 import os
+from botocore.exceptions import ClientError
 
 # Specify your AWS credentials (or use IAM role if running on EC2 instance with correct permissions)
+# iam_user_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+# iam_user_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+
 iam_user_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 iam_user_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 
 # Specify the AWS region and S3 bucket name
 region_name = 'ap-south-1'
 bucket_name = 'keerti-y.aws.s3.bucket1'
+object_name = 'test-bucket'
+file_data = 'keerti Y'
 
 
 # Create an S3 client
@@ -42,9 +48,45 @@ def append_object(bucket_name, key, data):
     # Upload updated data as a new object
     s3.put_object(Bucket=bucket_name, Key=key, Body=updated_data.encode('utf-8'))
 
+
+#     try:
+#         # Check if the object already exists
+#         s3.head_object(Bucket=bucket_name, Key=object_name)
+#         print(f"Object '{object_name}' already exists in bucket '{bucket_name}'")
+#         return object_name
+#     except ClientError as e:
+#         # If the object does not exist, create it
+#         if e.response['Error']['Code'] == '404':
+#             s3.put_object(Bucket=bucket_name, Key=object_name)
+#             print(f"Object '{object_name}' created in bucket '{bucket_name}'")
+#             return object_name
+#         else:
+#             # If another error occurs, print the error message
+#             print("An error occurred:", e.response['Error']['Message'])
+
+def create_s3_object(bucket_name, object_name, object_data):
+    # Put the object in the bucket
+    try:
+        response = s3.put_object(Bucket=bucket_name, Key=object_name, Body=file_data)
+        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+            print(f"Object '{object_name}' created in bucket '{bucket_name}'")
+            return True
+    except ClientError as e:
+        # Handle the exception
+        print(f"An error occurred: {e}")
+        return False
+
+
+
 # Example usage
 if __name__ == "__main__":
     # Read an object
+    success = create_s3_object(bucket_name, object_name, file_data)
+    if success:
+        print("Object creation successful!")
+    else:
+        print("Object creation failed.")
+
     print("List of all objects in S3 bucket:")
     all_objects = list_objects(bucket_name)
     for obj in all_objects:
@@ -67,5 +109,6 @@ if __name__ == "__main__":
         print("Read object from S3:")
         print(object_data)
 
+    
 
  
